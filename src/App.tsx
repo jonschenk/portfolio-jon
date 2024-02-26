a9d88c341d3f3f8d45c1e2b89662a2ced4d506a3
jonschenk <jonschenkjr@gmail.com>
2024-02-26 14:10:31 -0800

    remove divider line under github status+ adjusting

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
       <Introduction />
       <Divider />
       <GitHubStatus />
-      <Divider />
       {/* About container */}
       <MyFavoriteTools />
       {/* <Projects /> */}
