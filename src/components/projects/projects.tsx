de26586eecbfa0239e742c7763d850b2468b7bf1
jonschenk <jonschenkjr@gmail.com>
2024-03-13 01:25:01 -0700

    ui bugs

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -4,6 +4,7 @@ import "./projects.css";
 const Projects = () => {
   return (
     <div className="projects-container">
+      <h1>Experience & Projects</h1>
       <ProjectItem
         cat="Internship"
         title="Tanoshi Product Team Intern"
